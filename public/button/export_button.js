f414865553c6caf97e9db292fe83364414f1d8e7
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2016-08-29 20:13:50 +0200

    fix export sub-selector

--- public/button/export_button.js
+++ public/button/export_button.js
@@ -13,14 +13,49 @@ const linkReqRespStats = function ($scope, config) {
 
     const resp = $scope.req.resp;
 
+    $scope.resKeys = [];
+    $scope.iterateKeys = function(data) {
+ 	   var result = {};
+ 	   function recurse (cur, prop) {
+ 	       if (Object(cur) !== cur) {
+ 	           result[prop] = cur;
+ 	       } else if (Array.isArray(cur)) {
+ 	           result[prop] = cur;
+ 	       } else {
+ 	           var isEmpty = true;
+ 	           for (var p in cur) {
+ 	               isEmpty = false;
+ 	               recurse(cur[p], prop ? prop+"."+p : p);
+ 	           }
+ 	           if (isEmpty && prop)
+ 	               result[prop] = {};
+ 	       }
+ 	   }
+ 	   recurse(data, "");
+ 	   $scope.resKeys = result;
+    }
+
+    if ($scope.req.resp) $scope.iterateKeys($scope.req.resp);
+    else $scope.resKeys = [];
+
+    $scope.export_root;
+    $scope.filtered = $scope.req.resp;
+
+    $scope.updateRoot = function() {
+        var newdata = $scope.req.resp;
+    	var res = $scope.export_root.split(".")
+        res.forEach(function(node){ newdata = newdata[node] });
+   	$scope.filtered = newdata;
+    }
+
+
     function saveToJson(data) {
         const blob = new Blob([JSON.stringify(data)], {type: 'application/json'});
         saveAs(blob, 'response_export.json');
     };
 
     $scope.exportJson = function() {
-	// server.log(['info','status','export'], 'Saving response as JSON... ');
-          saveToJson(resp);
+          saveToJson($scope.filtered ? $scope.filtered : resp);
     };
 
     function saveToCsv(data) {
@@ -29,8 +64,7 @@ const linkReqRespStats = function ($scope, config) {
     };
 
     $scope.exportCsv = function() {
-	// server.log(['info','status','export'], 'Saving response as CSV... ');
-	jsonexport(resp,function(err, csv){
+	jsonexport($scope.filtered ? $scope.filtered : resp,function(err, csv){
 	    if(err) return console.log(err);
             saveToCsv(csv);
 	});
